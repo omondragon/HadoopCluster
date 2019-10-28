bfbc77afa78844232fc5895b2ca8872f2e0dbe5f
omondragon <oscar.mondragon@gmail.com>
2019-10-28 11:30:46 -0500

    fixing typo

--- scripts/setup-node.sh
+++ scripts/setup-node.sh
@@ -12,9 +12,9 @@ function hadoopUser {
 function installTools {
 	yum install -y java-1.8.0-openjdk-devel
 	cat > /etc/hosts <<EOF
-192.168.92.10 nodemasterx
-192.168.92.11 nodea
-192.168.92.12 nodeb
+192.168.70.10 nodemasterx
+192.168.70.11 nodea
+192.168.70.12 nodeb
 EOF
 
 }
